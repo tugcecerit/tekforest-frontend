739a6547762b44c1461d255611194eb911a2f4da
ericareven <erica.reven@gmail.com>
2023-04-12 19:10:50 -0400

    register and signin components and updated app.js

--- src/pages/Register.js
+++
@@ -1,9 +0,0 @@
-const Register = (props) => {
-    return (
-        <div>
-            <h2>register page</h2>
-        </div>
-    )
-}
-
-export default Register;
--- src/pages/Signin.js
+++
@@ -1,9 +0,0 @@
-const Signin = (props) => {
-    return (
-        <div>
-            <h2>sign in page</h2>
-        </div>
-    )
-}
-
-export default Signin;
