e2d133daade6fba7f541a85f09c7b56f2ee945ee
tugce <tugcetopkaccerit@gmail.com>
2023-04-15 15:33:33 -0400

    worked on some styling on some pages and navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,24 +34,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import PlantCategories from '../pages/PlantCategories';
-import PlantCard from './PlantCard';
 
 const Header = (props) => {
   return (
     <Navbar className='nav'>
       <Link to='/' className='nav-link link'>
-        <Navbar.Brand>HOME</Navbar.Brand>
+        <Navbar.Brand><img className='logo' src="/logo2.png"></img></Navbar.Brand>
       </Link>
       <Navbar.Collapse>
         <Nav>
-          <Link to='/plants' className='link'>
-            PLANTS
-          </Link>
-          <NavDropdown title='CATEGORIES' className='category-link'>
+          <div className='link-container'>
+          <div className='dropdown'>
+          <NavDropdown title={<span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-down-fill" viewBox="0 0 16 16">
+              <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+            </svg> CATEGORIES</span>} className='category-link'>
             <div className='category-dropdown'>
               <PlantCategories />
             </div>
           </NavDropdown>
+          </div>
+          <div className='other-links'>
+          <Link to='/plants' className='link'>
+            PLANTS
+          </Link>
           <Link to='/about' className='link'>
             ABOUT US
           </Link>
@@ -62,8 +67,10 @@ const Header = (props) => {
             REGISTER
           </Link>
           <Link to='/signin' className='link'>
-            SIGN IN
+            SIGN IN 
           </Link>
+          </div>
+          </div>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = (props) => {
                 <h3>Welcome to our plant paradise! Are you looking to add some greenery to your room? Look no further. Our website offers a wide selection of beautiful plants that are perfect for any room in your home.</h3>
                 <Button to="/plants" label="PLANTS" />
                 <Button to="/register" label="JOIN" />
-                <img className="home-img" src="/home13.png"></img>
+                <img className="home-img" src="/home15.png"></img>
             </div>
         </div>
     )
