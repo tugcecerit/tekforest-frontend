59b05d11b49dc91acc0746e5e664d4b8f791814d
tugce <tugcetopkaccerit@gmail.com>
2023-04-15 11:24:43 -0400

    worked on dropdown menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,72 @@
+// import { Link } from 'react-router-dom';
+
+// const Header = (props) => {
+//   return (
+//     <nav className='nav'>
+//       <Link to="/" className='link'>
+//         <div>HOME</div>
+//       </Link>
+//       <Link to="/plants" className='link'>
+//         <div>PLANTS</div>
+//       </Link>
+//       <Link to="/categories" className='link'>
+//         <div>CATEGORIES</div>
+//       </Link>
+//       <Link to="/about" className='link'>
+//         <div>ABOUT US</div>
+//       </Link>
+//       <Link to="/contact" className='link'>
+//         <div>CONTACT US</div>
+//       </Link>
+//       <Link to="/register" className='link'>
+//         <div>REGISTER</div>
+//       </Link>
+//       <Link to="/signin" className='link'>
+//         <div>SIGN IN</div>
+//       </Link>
+//     </nav>
+//   );
+// }
+
+// export default Header;
+
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import PlantCategories from '../pages/PlantCategories';
+import PlantCard from './PlantCard';
 
 const Header = (props) => {
   return (
-    <nav className='nav'>
-      <Link to="/" className='link'>
-        <div>HOME</div>
+    <Navbar className='nav'>
+      <Link to='/' className='nav-link link'>
+        <Navbar.Brand>HOME</Navbar.Brand>
       </Link>
-      <Link to="/plants" className='link'>
-        <div>PLANTS</div>
-      </Link>
-      <Link to="/categories" className='link'>
-        <div>CATEGORIES</div>
-      </Link>
-      <Link to="/about" className='link'>
-        <div>ABOUT US</div>
-      </Link>
-      <Link to="/contact" className='link'>
-        <div>CONTACT US</div>
-      </Link>
-      <Link to="/register" className='link'>
-        <div>REGISTER</div>
-      </Link>
-      <Link to="/signin" className='link'>
-        <div>SIGN IN</div>
-      </Link>
-    </nav>
+      <Navbar.Collapse>
+        <Nav>
+          <Link to='/plants' className='link'>
+            PLANTS
+          </Link>
+          <NavDropdown title='CATEGORIES' className='category-link'>
+            <div className='category-dropdown'>
+              <PlantCategories />
+            </div>
+          </NavDropdown>
+          <Link to='/about' className='link'>
+            ABOUT US
+          </Link>
+          <Link to='/contact' className='link'>
+            CONTACT US
+          </Link>
+          <Link to='/register' className='link'>
+            REGISTER
+          </Link>
+          <Link to='/signin' className='link'>
+            SIGN IN
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
