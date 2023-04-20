2a240bc8e756e6984e062919049b5603d2a6c0bb
tugce <tugcetopkaccerit@gmail.com>
2023-04-19 22:08:35 -0400

    backend URL added

--- src/components/Main.js
+++ src/components/Main.js
@@ -165,7 +165,6 @@ const Main = (props) => {
       useEffect(() => {
         getPlants()
         getUserPlants()
-        console.log(getPlants)
     }, [])
     return (
         <authContext.Provider
