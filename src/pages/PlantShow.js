cee91fc2f5870dfd04cf1220665a641882c3e140
Kevin Compton <kevincomptondev@gmail.com>
2023-04-21 18:46:00 -0400

    Plant show page css

--- src/pages/PlantShow.js
+++ src/pages/PlantShow.js
@@ -9,22 +9,24 @@ const PlantShow= (props) => {
 
     const loaded = () => {
         return (
-            <div className="plant-show container">
-                <div className="row">
-                    <div className="col-12 col-md-6">
-                        <img className="plant-show-image"  src={plant.Img}/>
-                    </div>
-                    <div className="plant-show-data col-12 col-md-6">
-                        <h1 className="plant-show-name">{plant['Common name']}</h1>
-                        <p><span>Categories: </span>{plant['Categories']}</p>
-                        <p><span>Climat: </span>{plant['Climat']}</p>
-                        <p><span>Common name (fr.): </span>{plant['Common name (fr.)']}</p>
-                        <p><span>Latin name: </span>{plant['Latin name']}</p>
-                        <p><span>Description: </span>{plant['Description']}</p>
-                        <p><span>Family: </span>{plant['Family']}</p>
-                        <p><span>Origin: </span>{plant['Origin']}</p>
-                        <p><span>Other names: </span>{plant['Other names']}</p>
-                        <p><span>Zone: </span>{plant['Zone']}</p>
+            <div className="plant-show">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-6">
+                            <img className="plant-show-image"  src={plant.Img}/>
+                        </div>
+                        <div className="plant-show-data col-12 col-md-6">
+                            <h1 className="plant-show-name">{plant['Common name']}</h1>
+                            <p><span>Categories: </span>{plant['Categories']}</p>
+                            <p><span>Climat: </span>{plant['Climat']}</p>
+                            <p><span>Common name (fr.): </span>{plant['Common name (fr.)']}</p>
+                            <p><span>Latin name: </span>{plant['Latin name']}</p>
+                            <p><span>Description: </span>{plant['Description']}</p>
+                            <p><span>Family: </span>{plant['Family']}</p>
+                            <p><span>Origin: </span>{plant['Origin']}</p>
+                            <p><span>Other names: </span>{plant['Other names']}</p>
+                            <p><span>Zone: </span>{plant['Zone']}</p>
+                        </div>
                     </div>
                 </div>
             </div>
