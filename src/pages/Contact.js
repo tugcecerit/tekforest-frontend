18a58e1da18725bbd4c6c19d39e821a73f73a646
tugce <tugcetopkaccerit@gmail.com>
2023-04-21 18:25:25 -0400

    pushing changes to switch branch

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,19 +8,17 @@ const Contact = (props) => {
         window.location.href = '/';
     }
     return (
-        <div className='contact-us'>  
-        <div className='contact-us-content'>      
+        <div className='contact-us'>           
             <h1>CONTACT US</h1> 
-            <div className='contact-us-form'>
-            <form className="pure-form pure-form-aligned pure-u-1 pure-u-md-1-2">
+            <form className="pure-form pure-form-aligned">
                 <fieldset>
                     <div className="pure-control-group">
                         <label htmlFor="aligned-name">Name</label>
                         <input type="text" className="pure-input-1-4" id="aligned-name" placeholder="Name" />
                     </div><br></br>
                     <div className="pure-control-group">
-                        <label htmlFor="aligned-tel">Phone Number</label>
-                        <input type="tel" className="pure-input-1-4" id="aligned-tel" placeholder="Phone Number" />
+                        <label htmlFor="aligned-name">Phone Number</label>
+                        <input type="tel" className="pure-input-1-4" id="aligned-name" placeholder="Phone Number" />
                     </div><br></br>
                     <div className="pure-control-group">
                         <label htmlFor="aligned-email">Email Address</label>
@@ -28,7 +26,7 @@ const Contact = (props) => {
                     </div><br></br>
                     <div className="pure-control-group">
                         <label htmlFor="aligned-foo">Message</label>
-                        <textarea className="pure-input-1-4" id="aligned-foo" placeholder="Message" rows="5"></textarea>
+                        <textarea className="pure-input-1-4" id="aligned-foo" placeholder="Message" rows="5" ></textarea>
                     </div><br></br>
                     <div className="pure-controls">
                         <label htmlFor="aligned-cb" className="pure-checkbox">
@@ -38,8 +36,6 @@ const Contact = (props) => {
                     </div>
                 </fieldset>
             </form>
-            </div>
-            </div>   
         </div>
     )
 }
