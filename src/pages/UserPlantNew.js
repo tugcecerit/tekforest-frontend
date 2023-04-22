980c6e7bd4deb6c95cbc7487c090934c8431eebc
Kevin Compton <kevincomptondev@gmail.com>
2023-04-21 22:23:44 -0400

    User Plant new page css

--- src/pages/UserPlantNew.js
+++ src/pages/UserPlantNew.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import './UserPlantNew.css';
 
 const UserPlantNew = (props) => {
     const navigate = useNavigate()
@@ -26,30 +27,134 @@ const UserPlantNew = (props) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                value={form.commonName}
-                name="commonName"
-                placeholder="Common Name"
-                onChange={handleChanges}
-            />
-            <input
-                type="text"
-                value={form.image}
-                name="image"
-                placeholder="Image URL"
-                onChange={handleChanges}
-            />
-            <input
-                type="text"
-                value={form.category}
-                name="category"
-                placeholder="Category"
-                onChange={handleChanges}
-            />
-            <input type="submit" value="Create Plant" />
-        </form>
+        <div className="container">
+            <h1 className="plant-new-name">New Plant</h1>
+            <form onSubmit={handleSubmit} className="pure-form pure-form-stacked">
+                        <label htmlFor="nickname">Name</label>
+                        <input
+                            id="nickname"
+                            className="pure-u-1"
+                            type="text"
+                            value={form.nickname}
+                            name="nickname"
+                            placeholder="Plant Name"
+                            className="pure-input-1"
+                            onChange={handleChanges}
+                        />
+                        <label htmlFor="image">Image</label>
+                        <input
+                            id="image"
+                            className="pure-u-1"
+                            type="text"
+                            value={form.image}
+                            name="image"
+                            placeholder="Image Link"
+                            className="pure-input-1"
+                            onChange={handleChanges}
+                        />
+                        <label htmlFor="size">Size</label>
+                        <input
+                            id="size"
+                            className="pure-u-1"
+                            type="text"
+                            value={form.size}
+                            name="size"
+                            placeholder="Plant Size"
+                            className="pure-input-1"
+                            onChange={handleChanges}
+                        />
+                        <label htmlFor="description">Description</label>
+                        <textarea
+                            id="description"
+                            className="pure-u-1"
+                            value={form.description}
+                            name="description"
+                            placeholder="Description"
+                            className="pure-input-1"
+                            onChange={handleChanges}
+                        ></textarea>
+                        <label htmlFor="commonName">Common Name</label>
+                        <div className="hidden-fields">
+                            <input
+                                id="commonName"
+                                className="pure-u-1"
+                                type="text"
+                                value={form.commonName}
+                                name="commonName"
+                                placeholder="Common Name"
+                                className="pure-input-1"
+                                onChange={handleChanges}
+                            />
+                            <label htmlFor="climat">Climat</label>
+                            <input
+                                id="climat"
+                                className="pure-u-1"
+                                type="text"
+                                value={form.climat}
+                                name="climat"
+                                placeholder="Climat"
+                                className="pure-input-1"
+                                onChange={handleChanges}
+                            />
+                            <label htmlFor="category">Category</label>
+                            <input
+                                id="category"
+                                className="pure-u-1"
+                                type="text"
+                                value={form.category}
+                                name="category"
+                                placeholder="Category"
+                                className="pure-input-1"
+                                onChange={handleChanges}
+                            />
+                            <label htmlFor="family">Family</label>
+                            <input
+                                id="family"
+                                className="pure-u-1"
+                                type="text"
+                                value={form.family}
+                                name="family"
+                                placeholder="Family"
+                                className="pure-input-1"
+                                onChange={handleChanges}
+                            />
+                            <label htmlFor="origin">Origin</label>
+                            <input
+                                id="origin"
+                                className="pure-u-1"
+                                type="text"
+                                value={form.origin}
+                                name="origin"
+                                placeholder="Origin"
+                                className="pure-input-1"
+                                onChange={handleChanges}
+                            />
+                            <label htmlFor="latinName">Latin Name</label>
+                            <input
+                                id="latinName"
+                                className="pure-u-1"
+                                type="text"
+                                value={form.latinName}
+                                name="latinName"
+                                placeholder="Latin Name"
+                                className="pure-input-1"
+                                onChange={handleChanges}
+                            />
+                            <label htmlFor="otherNames">Other Names</label>
+                            <input
+                                id="otherNames"
+                                className="pure-u-1"
+                                type="text"
+                                value={form.otherNames}
+                                name="otherNames"
+                                placeholder="Latin Name"
+                                className="pure-input-1"
+                                onChange={handleChanges}
+                            />
+                        </div>
+                    <input className="new-button" type="submit" value="Create Plant" />
+            </form>
+        </div>
     )
 }
 
